a6d80381350bad8e61bcce31ecfe75e10c16a124
jverdad <jian.verdad@gmail.com>
2024-04-26 13:39:26 -0700

    created journal form linked to DB

--- journals/views.py
+++ journals/views.py
@@ -5,10 +5,11 @@ from django.views import generic
 from django.utils import timezone
 from .models import Journal
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .journal_form import JournalForm
 
 # Journal views
 class IndexView(generic.ListView):
@@ -20,8 +21,24 @@ class DetailView(generic.DetailView):
     template_name = 'journals/detail.html'
 
 #define custom view for the journal page
-class JournalEntriesView(TemplateView):
-    template_name = 'journals/Journal Entries.html'
+# class JournalEntriesView(TemplateView):
+#     template_name = 'journals/Journal Entries.html'
+
+#TEST JOURNAL ENTRIES REQUEST RENDER
+def JournalEntries(request):
+    submitted = False
+    if(request.method == "POST"):
+        form = JournalForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/journals/journal-entries?submitted=True')
+    else:
+        form = JournalForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    
+    return render(request, 'journals/Journal Entries.html', {'form':form, 'submitted':submitted})
 
 
 #define custom view for the login page when log out
