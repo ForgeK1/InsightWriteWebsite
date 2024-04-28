0d32b5ef4655af705d5cc1ce033c82d5d0c1db97
Keyvan Mahmoodzadeh Kani <arasanjkk@gmail.com>
2024-04-28 10:43:10 -0700

    New Models and created an Analytics App
    So I did the following things to update the database:
    
    1) Created a Tag model under the Journals App
    
    2) Created an Analytics App and then created a Analytics model after

--- website/settings.py
+++ website/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'analytics',
     'journals',
     'login_signup',
     'meditations',
