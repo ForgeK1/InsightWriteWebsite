5db42cdfb59298dc1625e8f77fc257f64efde8dd
jverdad <jian.verdad@gmail.com>
2024-05-03 16:55:27 -0700

    Updated Journals Form, auto-add dates for journal creation

--- journals/models.py
+++ journals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Journal Models
 
@@ -7,8 +8,8 @@ class Journal(models.Model):
     user_id = models.IntegerField()
     tag_id = models.CharField()
     title = models.CharField(max_length=200)
-    entry_date = models.DateField('Date published')
-    last_modified_date = models.DateField('Last modified')
+    entry_date = models.DateField(auto_now_add=True, verbose_name='Date published')
+    last_modified_date = models.DateField(auto_now=True, verbose_name='Last modified')
     content = models.TextField() 
 
 class Tag(models.Model):
