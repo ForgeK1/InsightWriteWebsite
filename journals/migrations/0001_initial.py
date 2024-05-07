d04656f6d09e5643d57bf5968fb4b3c74fc14292
jverdad <jian.verdad@gmail.com>
2024-05-07 14:21:19 -0700

    Fixed duplicate models and fields

--- journals/migrations/0001_initial.py
+++ journals/migrations/0001_initial.py
@@ -15,24 +15,12 @@ class Migration(migrations.Migration):
             name='Journal',
             fields=[
                 ('journal_id', models.AutoField(primary_key=True, serialize=False)),
-                ('journal_id', models.AutoField(primary_key=True, serialize=False)),
                 ('user_id', models.IntegerField()),
                 ('tag_id', models.CharField()),
                 ('title', models.CharField(max_length=200)),
                 ('entry_date', models.DateField(auto_now_add=True, verbose_name='Date published')),
                 ('last_modified_date', models.DateField(auto_now=True, verbose_name='Last modified')),
-                ('entry_date', models.DateField(auto_now_add=True, verbose_name='Date published')),
-                ('last_modified_date', models.DateField(auto_now=True, verbose_name='Last modified')),
                 ('content', models.TextField()),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Tag',
-            fields=[
-                ('tag_id', models.IntegerField(primary_key=True, serialize=False, verbose_name='Tag ID')),
-                ('journal_id', models.IntegerField()),
-                ('tag_name', models.CharField(max_length=200)),
-                ('tag_color', models.CharField(max_length=10)),
             ],
         ),
         migrations.CreateModel(
